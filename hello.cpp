e04970022ea96073aa98d1a7e4ea64b03113d5a7
Padraic Calpin <padraic.calpin93@gmail.com>
2017-01-10 15:02:06 +0000

    Greetee passable from command line

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 int main(int argc, char** argv)
 {
-  std::cout << "Hello World!" << std::endl;
+  //Making some dumb changes
+  // std::cout << "Hello World!" << std::endl;
+  if (argc==1){
+    puts("Hello, World!");
+  }
+  else {
+    printf("Hello, %s\n", argv[1]);
+  }
   return 0;
 }
